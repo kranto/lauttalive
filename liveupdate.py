6789bf8feeb568ae053cd8dc0f425cf73ab4fdc3
Kyösti Ranto <kranto@gmail.com>
2018-09-26 20:41:51 +0300

    updated liveupdate.py. not functional atm

--- liveupdate.py
+++ liveupdate.py
@@ -6,9 +6,11 @@ import io
 from time import time,sleep
 import urllib2
 
-rest_base_uri = 'http://meri-aws-test.digitraffic.fi/api/v1'
-mqtt_uri = 'meri-aws-mqtt-test.digitraffic.fi'
+rest_base_uri = 'https://meri-aws-test.digitraffic.fi/api/v1'
+mqtt_host = 'meri-aws-mqtt-test.digitraffic.fi'
+mqtt_host = 'b-afca9ce3-f38d-459d-b495-43d879decdaa-2.mq.eu-west-1.amazonaws.com'
 mqtt_port = 9001
+mqtt_port = 61619
 
 vessels = {}
 locations = {}
@@ -16,88 +18,88 @@ locations = {}
 subscribed = False
 
 def connect():
-	client.connect(mqtt_uri, mqtt_port, 60)
-	client.loop_start()
+    client.connect(mqtt_host, mqtt_port, 60)
+    client.loop_start()
 
 def on_connect(client, userdata, flags, rc):
     print "on_connect"
-    if not subscribed:
-	    client.subscribe("vessels/#")
-	    subscribed = True
+    client.subscribe("vessels/230987870/#")
 
 def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
     handle_message(msg.payload)
 
 def is_passanger_vessel(metadata):
-	return metadata["shipType"] == 0 or metadata["shipType"] == 60
+    return metadata["shipType"] == 0 or metadata["shipType"] == 60
 
 def is_in_area(feature):
-	coords = feature["geometry"]["coordinates"]
-	return 18 <= coords[0] <= 23 and 59 <= coords[1] <= 61
+    coords = feature["geometry"]["coordinates"]
+    return 18 <= coords[0] <= 23 and 59 <= coords[1] <= 61
 
 def store_metadata(metadata):
-	#print "meta", json.dumps(metadata)
-	vessels[metadata["mmsi"]] = metadata
+    #print "meta", json.dumps(metadata)
+    vessels[metadata["mmsi"]] = metadata
 
 def handle_metadata(metadata):
-	if is_passanger_vessel(metadata):
-		store_metadata(metadata)
+    if is_passanger_vessel(metadata):
+        store_metadata(metadata)
 
 def store_location(feature):
-	feature["properties"]["mmsi"] = feature["mmsi"]
-	feature["properties"]["vessel"] = vessels[feature["mmsi"]]
-	locations[feature["mmsi"]] = feature
+    feature["properties"]["mmsi"] = feature["mmsi"]
+    feature["properties"]["vessel"] = vessels[feature["mmsi"]]
+    locations[feature["mmsi"]] = feature
 
 def handle_location(feature):
-	locations.pop(feature["mmsi"], None)
-	if is_in_area(feature) and feature["mmsi"] in vessels:
-		print (time() - feature["properties"]["timestampExternal"]/1000), vessels[feature["mmsi"]]["name"]
-		store_location(feature)
+    locations.pop(feature["mmsi"], None)
+    if is_in_area(feature) and feature["mmsi"] in vessels:
+        print (time() - feature["properties"]["timestampExternal"]/1000), vessels[feature["mmsi"]]["name"]
+        store_location(feature)
 
 def handle_message(msg):
-	data = yaml.safe_load(msg)
-	if "shipType" in data:
-		handle_metadata(data)
-	elif "geometry" in data:
-		handle_location(data)
+    data = yaml.safe_load(msg)
+    if "shipType" in data:
+        handle_metadata(data)
+    elif "geometry" in data:
+        handle_location(data)
 
 def initial_load():
-	metadata_json = urllib2.urlopen(rest_base_uri + "/metadata/vessels").read()
-	for metadata in yaml.safe_load(metadata_json): 
-		handle_metadata(metadata)
-	location_json = urllib2.urlopen(rest_base_uri + "/locations/latest").read()
-	feature_collection = yaml.safe_load(location_json)
-	for feature in feature_collection["features"]: 
-		handle_location(feature)
+    metadata_json = urllib2.urlopen(rest_base_uri + "/metadata/vessels").read()
+    for metadata in yaml.safe_load(metadata_json): 
+        handle_metadata(metadata)
+    location_json = urllib2.urlopen(rest_base_uri + "/locations/latest").read()
+    feature_collection = yaml.safe_load(location_json)
+    for feature in feature_collection["features"]: 
+        handle_location(feature)
 
 def initial_load2():
-	r = requests.get(rest_base_uri + '/metadata/vessels')
-	for metadata in yaml.safe_load(r.text): 
-		handle_metadata(metadata)
-	r = requests.get(rest_base_uri + '/locations/latest')
-	feature_collection = yaml.safe_load(r.text)
-	for feature in feature_collection["features"]: 
-		handle_location(feature)
+    r = requests.get(rest_base_uri + '/metadata/vessels')
+    for metadata in yaml.safe_load(r.text): 
+        handle_metadata(metadata)
+    r = requests.get(rest_base_uri + '/locations/latest')
+    feature_collection = yaml.safe_load(r.text)
+    for feature in feature_collection["features"]: 
+        handle_location(feature)
 
 def output_locations():
-	now = int(time()*1000)
-	features = [f for f in locations.values() if (now - f["properties"]["timestampExternal"])/1000 <= 600]
-	feature_collection = {"features": features}
-	with io.open('livedata.json1', 'w') as f:
-  		f.write(unicode(json.dumps(feature_collection).encode('UTF-8')))
+    now = int(time()*1000)
+    features = [f for f in locations.values() if (now - f["properties"]["timestampExternal"])/1000 <= 600]
+    feature_collection = {"features": features}
+    with io.open('livedata.json1', 'w') as f:
+        f.write(unicode(json.dumps(feature_collection).encode('UTF-8')))
 
 def output_locations_forever():
-	while True:
-		output_locations()
-		sleep(10)
+    while True:
+        output_locations()
+        sleep(10)
 
 # --
 
 client = mqtt.Client(transport="websockets")
 client.on_connect = on_connect
 client.on_message = on_message
+client.tls_set()
+    
 
-initial_load()
+#initial_load()
 connect()
 output_locations_forever()
